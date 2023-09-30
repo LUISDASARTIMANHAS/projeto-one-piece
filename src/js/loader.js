3cd0c5763671c9c4297215c10f6808272df3a299
LUIS DAS ARTIMANHAS <luisaugustodesouza785@gmail.com>
2023-09-30 20:43:12 -0300

    updated js live 3

---
+++ src/js/loader.js
@@ -0,0 +1,40 @@
+const path = "projetos/one-piece=personagens";
+const urlGetDados = "https://pingobras-sg.glitch.me/" + path;
+
+function getData() {
+    const options = {
+        method: "GET",
+        mode: "cors",
+        headers: {
+            "content-type": "application/json;charset=utf-8",
+            "Authorization": "snve072509ç$",
+        }
+    };
+
+
+    fetch(urlGetDados, options)
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                return response.text()
+            }
+        })
+        .then((data) => {
+            console.log("DATA RESPONSE: ");
+            console.log(data);
+            loader(data);
+        })
+        .catch((error) => erro(error));
+}
+getData()
+
+
+function erro(error) {
+    console.debug(error);
+    alert(error)
+}
+
+function loader(){
+
+}
