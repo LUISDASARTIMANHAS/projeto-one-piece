0b5155fef9997eb85449535b9b09cb7c1f47021b
LUIS DAS ARTIMANHAS <luisaugustodesouza785@gmail.com>
2023-09-30 23:21:50 -0300

    updated bugs

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,27 @@
-const btns = document.querySelectorAll(".btn");
-const personagens = document.querySelectorAll(".person")
+setTimeout(sys, 5000)
 
-personagens.forEach(personagem => {
+function sys() {
+    const btns = document.querySelectorAll(".btn");
+    const personagens = document.querySelectorAll(".person")
 
-});
-btns.forEach((btn, i) => {
-    btn.addEventListener("click", () => {
-        const personagem = personagens[i];
-        deselecionar()
-        btn.classList.add("select")
-        personagem.classList.add("select")
+    personagens.forEach(personagem => {
+
+    });
+    btns.forEach((btn, i) => {
+        btn.addEventListener("click", () => {
+            const personagem = personagens[i];
+            desselecionar()
+            btn.classList.add("select")
+            personagem.classList.add("select")
+        })
     })
-})
 
-function deselecionar() {
-    const btnSelect = document.querySelector(".btn.select");
-    const personagemSelect = document.querySelector(".person.select");
+    function desselecionar() {
+        const btnSelect = document.querySelector(".btn.select");
+        const personagemSelect = document.querySelector(".person.select");
 
-    personagemSelect.classList.remove("select");
-    btnSelect.classList.remove("select");
+        personagemSelect.classList.remove("select");
+        btnSelect.classList.remove("select");
+        console.log("Desselecionando elementos!")
+    }
 }
-
