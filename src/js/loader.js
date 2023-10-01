c5340a31f174a3a3720186190120f23d3dbffdec
LUIS DAS ARTIMANHAS <luisaugustodesouza785@gmail.com>
2023-09-30 22:37:47 -0300

    updated loader

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,5 +1,5 @@
-const path = "projetos/one-piece";
-const urlGetDados = "https://pingobras-sg.glitch.me/" + path;
+const urlGetDados = "https://pingobras-sg.glitch.me/projetos/one-piece";
+const mainElements = document.querySelector(".personagens")
 
 function getData() {
     const options = {
@@ -7,7 +7,7 @@ function getData() {
         mode: "cors",
         headers: {
             "content-type": "application/json;charset=utf-8",
-            "Authorization": "snve072509ç$",
+            Authorization: "snve072509ç$",
         }
     };
 
@@ -35,6 +35,42 @@ function erro(error) {
     alert(error)
 }
 
-function loader(database){
+function loader(database) {
+    for (let i = 0; i < database.length; i++) {
+        const dataPersonagem = database[i];
+        const nomePersonagem = dataPersonagem.personagem
+        const imgPersonagem = dataPersonagem.imagem
+        const infoPersonagem = dataPersonagem.descricao
 
+
+        const personagemElement = document.createElement("div");
+        const imgElement = document.createElement("img");
+        const painelInfoElement = document.createElement("div");
+        const logoElement = document.createElement("i");
+        const h2Element = document.createElement("h2");
+        const pElement = document.createElement("p");
+
+        // configurações da imagem
+        imgElement.classList.add("img");
+        imgElement.src = "./src/img/" + imgPersonagem
+        imgElement.alt = "Personagem " + nomePersonagem
+
+        // configurações da logo, nome do personagem e descrição
+        logoElement.classList.add("logo")
+        h2Element.classList.add("nome-person")
+        pElement.classList.add("info")
+        h2Element.innerText = nomePersonagem
+        pElement.innerText = infoPersonagem
+
+        // configurações do painel info
+        painelInfoElement.classList.add("painel-info")
+        painelInfoElement.appendChild(logoElement)
+        painelInfoElement.appendChild(h2Element)
+        painelInfoElement.appendChild(pElement)
+
+        personagemElement.appendChild(imgElement)
+        personagemElement.appendChild(painelInfoElement)
+
+        mainElements.appendChild(personagemElement);
+    }
 }
